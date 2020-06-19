da4a1b373327c2619055a089b9122a50f79c1ce0
Felix Schütt <felix.schuett@maps4print.com>
2020-06-19 20:07:38 +0200

    Use libloading-mini

--- generate.py
+++ generate.py
@@ -7,17 +7,10 @@ def write_file(string, path):
     text_file.close()
 
 code = ""
-code += "extern crate libloading;\r\n"
-
+code += "extern crate libloading_mini;\r\n"
 code += "\r\n"
-code += "\r\n"
-
-code += "#[cfg(unix)]\r\n"
-code += "use libloading::os::unix::{Library, Symbol};\r\n"
-code += "#[cfg(windows)]\r\n"
-code += "use libloading::os::windows::{Library, Symbol};\r\n"
-
-code += "\r\n"
+code += "use libloading_mini::Library;\r\n"
+code += "use std::mem;\r\n"
 code += "\r\n"
 
 for i in range(0, amount_of_structs):
@@ -30,21 +23,22 @@ code += "#[repr(C)]\r\n"
 code += "pub struct BigDll {\r\n"
 code += "    lib: Library,\r\n"
 for i in range(0, amount_of_structs - 1):
-    code += "    function_" + str(i) + ": Symbol<extern fn(_:  _" + str(i) + ") -> _" + str(i + 1) + ">,\r\n"
+    code += "    function_" + str(i) + ": fn(_:  _" + str(i) + ") -> _" + str(i + 1) + ",\r\n"
 code += "}\r\n"
 
 code += "\r\n"
 code += "\r\n"
 
-code += "pub fn load_big_dll(path: &str) -> Result<BigDll, &'static str> {\r\n"
-code += "    let lib = Library::new(path).map_err(|_| \"library is not a DLL file (?!)\")?;\r\n"
+code += "pub fn load_big_dll(path: &str) -> Option<BigDll> {\r\n"
+code += "    unsafe {\r\n"
+code += "        let lib = Library::new(path)?;\r\n"
+code += "        let dll = BigDll {\r\n"
+code += "            lib,\r\n"
 for i in range(0, amount_of_structs - 1):
-    code += "    let function_" + str(i) + " = unsafe { lib.get::<extern fn(_: _" + str(i) + ") -> _" + str(i + 1) + ">(b\"function_" + str(i) + "\").map_err(|_| \"could not load fn function_" + str(i) + "\")? };\r\n"
-code += "    Ok(BigDll {\r\n"
-code += "        lib,\r\n"
-for i in range(0, amount_of_structs - 1):
-    code += "        function_" + str(i) + ",\r\n"
-code += "    })\r\n"
+    code += "            function_" + str(i) + ": mem::transmute(lib.get(b\"function_" + str(i) + "\")?),\r\n"
+code += "        };\r\n"
+code += "        Some(dll)\r\n"
+code += "    }\r\n"
 code += "}\r\n"
 
 write_file(code, './src/lib.rs')
